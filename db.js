ee4403f80980a7ed3287e12ac2c3072f7d0d9bec
Bogdan Pshonyak <bpshonyak@live.com>
2017-01-17 22:54:28 -0800

    added user and post fixtures

--- db.js
+++ db.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+var Faker = require('faker');
 var Sequelize = require('sequelize');
 
 const conn = new Sequelize('graph_ql', 'root', 'root', {
@@ -13,3 +15,58 @@ conn
   .catch(function (err) {
     console.log('Unable to connect to the database:', err);
   });
+
+
+// Define tables
+
+const User = conn.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  }
+});
+
+const Post = conn.define('post', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+// Set up relationships
+
+User.hasMany(Post);
+Post.belongsTo(User);
+
+// Sync with the database
+
+conn.sync({force:true}).then(() => {
+  // Create some fixtures
+  _.times(10, () => {
+    return User.create({
+      firstName: Faker.name.firstName(),
+      lastName: Faker.name.lastName(),
+      email: Faker.internet.email()
+    }).then( user => {
+      user.createPost({
+        title: `Post by ${user.firstName}`,
+        content: 'Content of the post...'
+      });
+    });
+  });
+});
